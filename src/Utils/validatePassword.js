445fc6ae978b954bb397dd045b423445298ca242
malikhurem01 <40504567+malikhurem01@users.noreply.github.com>
2022-03-16 01:18:59 +0100

    Delete src directory

--- src/Utils/validatePassword.js
+++
@@ -1,9 +0,0 @@
-const validatePassword = (password) => {
-  var regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-  if (password.match(regex)) {
-    return true;
-  }
-  return false;
-};
-
-export default validatePassword;
