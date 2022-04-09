17732a69164058151dbbce96a6c3a72a789075ba
malikhurem01 <40504567+malikhurem01@users.noreply.github.com>
2022-04-09 23:52:42 +0200

    Delete src directory

--- src/Data/Constants/login.js
+++
@@ -1,7 +0,0 @@
-const WRONG_CREDENTIALS_ERROR = "Wrong username or password, please try again.";
-
-const constants = {
-  WRONG_CREDENTIALS_ERROR,
-};
-
-module.exports = constants;
--- src/Data/Constants/sort.js
+++
@@ -1,9 +0,0 @@
-const BY_CREATION_DATE = "createdAt";
-const BY_EXPIRATION_DATE = "auctionDateEnd";
-
-const constants = {
-  BY_CREATION_DATE,
-  BY_EXPIRATION_DATE,
-};
-
-module.exports = constants;
